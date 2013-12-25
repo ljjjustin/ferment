7edc086f3f429bdc6948bb0cdfa3ab7880a49336
Jiajun Liu <jiajun@unitedstack.com>
2013-12-25 12:29:55 +0800

    change test image name

--- tools/upload-cirros-image.sh
+++ tools/upload-cirros-image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 topdir=$(cd $(dirname $0) && pwd)
-cirros_image=${topdir}/cirros-disk.img
-IMAGE_NAME="cirros-0.3.1-x86_64"
+cirros_image=${topdir}/cirros-image-x64.img
+IMAGE_NAME="cirros-image-x64"
 
 if [ ! -f "${cirros_image}" ]; then
     wget -O ${cirros_image} http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-disk.img
