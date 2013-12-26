01a0d0b5fa77f0f6c958ac3d1d8973fa05fab3e3
Jiajun Liu <jiajun@unitedstack.com>
2013-12-26 14:37:42 +0800

    optimize logic judge statement

--- openstack/deploy.sh
+++ openstack/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-usage() {
-    echo "usage: $0 <command> <service>"
-    echo "<command>: [install, config, start, stop, restart, remove, reinstall]"
-    echo "<service>: [mysql, rabbitmq, keystone, glance, cinder, nova]"
-    exit
-}
-
 ALL_COMMANDS=('install' 'config' 'start' 'stop' 'restart' 'remove' 'reinstall')
 ALL_SERVICES=('mysql' 'rabbitmq' 'keystone' 'glance' 'cinder' 'nova')
+
+usage() {
+    echo "usage: $0 <command> <service>"
+    echo "<command>: [${ALL_COMMANDS[@]/%/,}]"
+    echo "<service>: [${ALL_SERVICES[@]/%/,}]"
+    exit
+}
 
 is_valid_command() {
     local command=$1
     for val in ${ALL_COMMANDS[@]}; do
-        if [ "${command}" = "${val}" ]; then
+        if [[ "${val}" == "${command}" ]]; then
             return 0
         fi
     done
@@ -23,14 +23,15 @@ is_valid_command() {
 is_valid_service() {
     local service=$1
     for val in ${ALL_SERVICES[@]}; do
-        if [ "${service}" = "${val}" ]; then
+        if [[ "${val}" == "${service}" ]]; then
             return 0
         fi
     done
     return 1
 }
+
 ## check parameters
-if [ $# -ne 2 ]; then
+if [[ $# -ne 2 ]]; then
     usage
 fi
 
@@ -42,30 +43,37 @@ is_valid_service ${service} || usage
 
 ## source functions
 topdir=$(cd $(dirname $0) && pwd)
-if [ -f "${topdir}/fermentrc" ]; then
+if [[ -f "${topdir}/fermentrc" ]]; then
     source ${topdir}/fermentrc
 fi
 source ${topdir}/lib/common
 source ${topdir}/lib/${service}
 
+## check necessary parameters
+if [[ -z "${SERVICE_HOST}" || -z "${HOST_ADDRESS}" ]]; then
+    echo "environment variable 'SERVICE_HOST' and 'HOST_ADDRESS' can not be null"
+    echo "set those two environment variable and run this scirpt again."
+    exit
+fi
+
 ## execute functions
-if [ "${command}" == "install" ]; then
+if [[ "install" == "${command}" ]]; then
     eval "install_${service}"
     eval "configure_${service}"
     eval "init_${service}"
-elif [ "${command}" == "config" ]; then
+elif [[ "config" == "${command}" ]]; then
     eval "configure_${service}"
-elif [ "${command}" == "start" ]; then
+elif [[ "start" == "${command}" ]]; then
     eval "start_${service}"
-elif [ "${command}" == "stop" ]; then
+elif [[ "stop" == "${command}" ]]; then
     eval "stop_${service}"
-elif [ "${command}" == "restart" ]; then
+elif [[ "restart" == "${command}" ]]; then
     eval "stop_${service}"
     eval "start_${service}"
-elif [ "${command}" == "remove" ]; then
+elif [[ "remove" == "${command}" ]]; then
     eval "stop_${service}"
     eval "cleanup_${service}"
-elif [ "${command}" == "reinstall" ]; then
+elif [[ "reinstall" == "${command}" ]]; then
     eval "stop_${service}"
     eval "cleanup_${service}"
     eval "install_${service}"
